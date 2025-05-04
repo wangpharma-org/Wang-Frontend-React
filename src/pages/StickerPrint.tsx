0063eeb039993413c9afcccf13238f60ac66df41
6710110400 SASIT SUANSADANG <155415427+Sasit-Nine@users.noreply.github.com>
2025-05-04 17:21:43 +0700

    Bot Print Complete

--- src/pages/StickerPrint.tsx
+++ src/pages/StickerPrint.tsx
@@ -26,7 +26,7 @@ const StickerPrint = () => {
   const [isOpen, setIsOpen] = useState<number[]>([]);
   const [pendingTickets, setPendingTickets] = useState<FloorInfo[]>([]);
   const [currentIndex, setCurrentIndex] = useState(0);
-  const [selectedRoute, setSelectedRoute] = useState('all');
+  const [selectedRoute, setSelectedRoute] = useState("all");
   useEffect(() => {
     const token = sessionStorage.getItem("access_token");
     console.log(token);
@@ -60,35 +60,35 @@ const StickerPrint = () => {
     console.log("listPrintTicket", listPrintTicket);
   }, [listPrintTicket]);
 
-  console.log("listPrintTicket "+listPrintTicket);
+  console.log("listPrintTicket " + listPrintTicket);
   const route = [
-    { route_name: "ทั้งหมด", value: "all"},
-    { route_name: "L1-1 หาดใหญ่", value: "หาดใหญ่"},
-    { route_name: "L1-2 สงขลา", value: "สงขลา"},
-    { route_name: "L1-3 สะเดา", value: "สะเดา"},
-    { route_name: "L1-5 สทิงพระ", value: "สทิงพระ"},
-    { route_name: "L10 นครศรีฯ", value: "นครศรีธรรมราช"},
-    { route_name: "L11 กระบี่", value: "กระบี่"},
-    { route_name: "L12 ภูเก็ต", value: "ภูเก็ต"},
-    { route_name: "L13 สุราษฏร์ธานี", value: "สุราษฎร์ธานี"},
+    { route_name: "ทั้งหมด", value: "all" },
+    { route_name: "L1-1 หาดใหญ่", value: "หาดใหญ่" },
+    { route_name: "L1-2 สงขลา", value: "สงขลา" },
+    { route_name: "L1-3 สะเดา", value: "สะเดา" },
+    { route_name: "L1-5 สทิงพระ", value: "สทิงพระ" },
+    { route_name: "L10 นครศรีฯ", value: "นครศรีธรรมราช" },
+    { route_name: "L11 กระบี่", value: "กระบี่" },
+    { route_name: "L12 ภูเก็ต", value: "ภูเก็ต" },
+    { route_name: "L13 สุราษฏร์ธานี", value: "สุราษฎร์ธานี" },
     { route_name: "L16 ยาแห้ง ส่งฟรี ทั่วไทย", value: "ยาแห้ง ส่งฟรี ทั่วไทย" },
-    { route_name: "L17 พังงา", value: "พังงา"},
-    { route_name: "L18 เกาะสมุย", value: "เกาะสมุย"},
-    { route_name: "L19 พัทลุง-นครฯ", value: "พัทลุง-นคร"},
-    { route_name: "L2 ปัตตานี", value: "ปัตตานี"},
-    { route_name: "L20 ชุมพร", value: "ชุมพร"},
-    { route_name: "L21 เกาะลันตา", value: "เกาะลันตา"},
-    { route_name: "L22 เกาะพะงัน", value: "เกาะพะงัน"},
-    { route_name: "L3 สตูล", value: "สตูล"},
-    { route_name: "L4 พัทลุง", value: "พัทลุง"},
-    { route_name: "L4-1 พัทลุง VIP", value: "พัทลุง VIP"},
-    { route_name: "L5-1 นราธิวาส", value: "นราธิวาส"},
-    { route_name: "L1-3 สุไหงโกลก", value: "สุไหงโกลก"},
-    { route_name: "L6 ยะลา", value: "ยะลา"},
-    { route_name: "L7 เบตง", value: "เบตง"},
-    { route_name: "L9 ตรัง", value: "ตรัง"},
-    { route_name: "L9-11 กระบี่-ตรัง", value: "กระบี่-ตรัง"},
-    { route_name: "Office รับเอง", value: "Office รับเอง"},
+    { route_name: "L17 พังงา", value: "พังงา" },
+    { route_name: "L18 เกาะสมุย", value: "เกาะสมุย" },
+    { route_name: "L19 พัทลุง-นครฯ", value: "พัทลุง-นคร" },
+    { route_name: "L2 ปัตตานี", value: "ปัตตานี" },
+    { route_name: "L20 ชุมพร", value: "ชุมพร" },
+    { route_name: "L21 เกาะลันตา", value: "เกาะลันตา" },
+    { route_name: "L22 เกาะพะงัน", value: "เกาะพะงัน" },
+    { route_name: "L3 สตูล", value: "สตูล" },
+    { route_name: "L4 พัทลุง", value: "พัทลุง" },
+    { route_name: "L4-1 พัทลุง VIP", value: "พัทลุง VIP" },
+    { route_name: "L5-1 นราธิวาส", value: "นราธิวาส" },
+    { route_name: "L1-3 สุไหงโกลก", value: "สุไหงโกลก" },
+    { route_name: "L6 ยะลา", value: "ยะลา" },
+    { route_name: "L7 เบตง", value: "เบตง" },
+    { route_name: "L9 ตรัง", value: "ตรัง" },
+    { route_name: "L9-11 กระบี่-ตรัง", value: "กระบี่-ตรัง" },
+    { route_name: "Office รับเอง", value: "Office รับเอง" },
   ];
   console.log("selectedRoute" + selectedRoute);
 
@@ -105,19 +105,18 @@ const StickerPrint = () => {
   };
 
   useEffect(() => {
-    if (selectFloor) {
+    if (selectFloor && listPrintTicket) {
       const floorKey = `F${selectFloor}`;
-      if (!listPrintTicket) return;
 
       const filtered = listPrintTicket
-        .map((item) => item[floorKey])
-        .filter(
-          (floorData) => floorData && floorData.print_status === "pending"
-        );
+        .map((item) => item[floorKey] || [])
+        .flat()
+        .filter((floorData) => floorData.print_status === "pending");
+
+      console.log("✅ Received ticket data:", filtered);
 
       setPendingTickets(filtered);
       setCurrentIndex(0);
-      //   setIsOpen([]);
     }
   }, [selectFloor, listPrintTicket]);
 
@@ -172,11 +171,6 @@ const StickerPrint = () => {
     return () => window.removeEventListener("storage", handleStorage);
   }, [pendingTickets, currentIndex, socket]);
 
-  const printTicket = (ticketId: number) => {
-    if (!ticketId) return;
-    window.open(`/format-sticker?ticketId=${ticketId}`, "_blank");
-  };
-
   if (loading)
     return (
       <div className="flex justify-center items-center h-screen">
@@ -208,7 +202,9 @@ const StickerPrint = () => {
           <button
             key={route.value}
             onClick={() => setSelectedRoute(route.value)}
-            className={`border-2 cursor-pointer border-blue-500 px-2 py-1 rounded-lg hover:bg-blue-500 hover:text-white transition ${route.value === selectedRoute ? "bg-blue-500 text-white" : ""}`}
+            className={`border-2 cursor-pointer border-blue-500 px-2 py-1 rounded-lg hover:bg-blue-500 hover:text-white transition ${
+              route.value === selectedRoute ? "bg-blue-500 text-white" : ""
+            }`}
           >
             {route.route_name}
           </button>
@@ -233,46 +229,63 @@ const StickerPrint = () => {
             </tr>
           </thead>
           <tbody className="divide-y divide-gray-200">
-            {(listPrintTicket || !loading) && 
+            {(listPrintTicket || !loading) &&
               listPrintTicket
-              .filter((list) => selectedRoute === 'all' || selectedRoute === '' || list.province === selectedRoute)
-              .map((list, index) => (
-                <tr key={index} className={`${list.picking_status==='picking' ? "bg-green-100 hover:bg-green-200" : "bg-white hover:bg-gray-50"}`}>
-                  <td className="px-6 py-4 text-center">{index + 1}</td>
-                  <td className="px-6 py-4 text-center">{list.mem_code}</td>
-                  <td className="px-6 py-4 text-center">{list.mem_name}</td>
-                  <td className="px-6 py-4 text-center">{list.province}</td>
+                .filter(
+                  (list) =>
+                    selectedRoute === "all" ||
+                    selectedRoute === "" ||
+                    list.province === selectedRoute
+                )
+                .map((list, index) => (
+                  <tr
+                    key={index}
+                    className={`${
+                      list.picking_status === "picking"
+                        ? "bg-green-100 hover:bg-green-200"
+                        : "bg-white hover:bg-gray-50"
+                    }`}
+                  >
+                    <td className="px-6 py-4 text-center">{index + 1}</td>
+                    <td className="px-6 py-4 text-center">{list.mem_code}</td>
+                    <td className="px-6 py-4 text-center">{list.mem_name}</td>
+                    <td className="px-6 py-4 text-center">{list.province}</td>
 
-                  {[2, 3, 4, 5].map((floor) => (
-                    <td
-                      key={floor}
-                      className={`px-6 py-4 ${getCellClass(
-                        list[`F${floor}`]?.print_status
-                      )}`}
-                    >
-                      <div className="flex justify-center items-center">
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          strokeWidth="1.5"
-                          stroke="currentColor"
-                          className="w-9 h-9 cursor-pointer hover:transform hover:scale-110 transition-all duration-200"
-                          onClick={() => {
-                            printTicket(list[`F${floor}`]?.ticket_id);
-                          }}
-                        >
-                          <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            d="M6.72 13.829c-.24.03-.48.062-.72.096m.72-.096a42.415 42.415 0 0 1 10.56 0m-10.56 0L6.34 18m10.94-4.171c.24.03.48.062.72.096m-.72-.096L17.66 18m0 0 .229 2.523a1.125 1.125 0 0 1-1.12 1.227H7.231c-.662 0-1.18-.568-1.12-1.227L6.34 18m11.318 0h1.091A2.25 2.25 0 0 0 21 15.75V9.456c0-1.081-.768-2.015-1.837-2.175a48.055 48.055 0 0 0-1.913-.247M6.34 18H5.25A2.25 2.25 0 0 1 3 15.75V9.456c0-1.081.768-2.015 1.837-2.175a48.041 48.041 0 0 1 1.913-.247m10.5 0a48.536 48.536 0 0 0-10.5 0m10.5 0V3.375c0-.621-.504-1.125-1.125-1.125h-8.25c-.621 0-1.125.504-1.125 1.125v3.659M18 10.5h.008v.008H18V10.5Zm-3 0h.008v.008H15V10.5Z"
-                          />
-                        </svg>
-                      </div>
-                    </td>
-                  ))}
-                </tr>
-              ))}
+                    {[2, 3, 4, 5].map((floor) => (
+                      <td
+                        key={floor}
+                        className={`px-6 py-4 ${getCellClass(
+                          list[`F${floor}`]?.some(
+                            (t) => t.print_status === "pending"
+                          )
+                            ? "pending"
+                            : list[`F${floor}`]?.some(
+                                (t) => t.print_status === "printed"
+                              )
+                            ? "printed"
+                            : "none"
+                        )}`}
+                      >
+                        <div className="flex justify-center items-center">
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth="1.5"
+                            stroke="currentColor"
+                            className="w-9 h-9 cursor-pointer"
+                          >
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              d="M6.72 13.829c-.24.03-.48.062-.72.096m.72-.096a42.415 42.415 0 0 1 10.56 0m-10.56 0L6.34 18m10.94-4.171c.24.03.48.062.72.096m-.72-.096L17.66 18m0 0 .229 2.523a1.125 1.125 0 0 1-1.12 1.227H7.231c-.662 0-1.18-.568-1.12-1.227L6.34 18m11.318 0h1.091A2.25 2.25 0 0 0 21 15.75V9.456c0-1.081-.768-2.015-1.837-2.175a48.055 48.055 0 0 0-1.913-.247M6.34 18H5.25A2.25 2.25 0 0 1 3 15.75V9.456c0-1.081.768-2.015 1.837-2.175a48.041 48.041 0 0 1 1.913-.247m10.5 0a48.536 48.536 0 0 0-10.5 0m10.5 0V3.375c0-.621-.504-1.125-1.125-1.125h-8.25c-.621 0-1.125.504-1.125 1.125v3.659M18 10.5h.008v.008H18V10.5Zm-3 0h.008v.008H15V10.5Z"
+                            />
+                          </svg>
+                        </div>
+                      </td>
+                    ))}
+                  </tr>
+                ))}
           </tbody>
         </table>
       </div>
