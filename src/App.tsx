7e61b0376e8b77dd34c0a6154a59dcf70a8bc26e
zZBANK22Zz <ananda2546@gmail.com>
2025-05-19 23:02:01 +0700

    Add Dashboard page.

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import ProductList from "./pages/ProductList";
 import StickerPrint from "./pages/StickerPrint";
 import FormatSticker from "./pages/FormatSticker";
 import EmployeeStatisticsPage from "./pages/EmployeeStatisticsPage";
+import DashboardQcPage from "./pages/DashboardQcPage";
 function App() {
   return (
     <>
@@ -128,6 +129,10 @@ function App() {
           <Route
             path="/employee-statistics"
             element={<EmployeeStatisticsPage />}
+          />
+          <Route
+            path="/dashboard-qc"
+            element={<DashboardQcPage />}
           />
         </Routes>
       </Router>
