aa4c2ccbad9de84df73a06aa5dd63045bcab35ea
XVPB511 <por4987@gmail.com>
2025-06-20 19:01:42 +0700

    feat: add button visibility logic to Home component and fetch data from API

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,6 +3,8 @@ import order from "../assets/sent.png";
 import statistics from "../assets/employees statistics.png";
 import checklist from "../assets/checklist.png";
 import { useNavigate } from "react-router";
+import { useEffect, useState } from "react";
+import axios from "axios";
 const listMenu = [
   {
     id: 1,
@@ -31,9 +33,29 @@ const listMenu = [
 ];
 const Home = () => {
   const navigate = useNavigate()
+  const [showButtonVerifyBill, setShowButtonVerifyBill] = useState(true)
   const handleNavigate = (link: string) => {
     navigate(link)
   }
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.post(`${import.meta.env.VITE_API_URL_VERIFY_ORDER}/api/verify/hide-button`);
+        setShowButtonVerifyBill(res.data.value);
+
+      } catch (error) {
+        console.error('Error fetching button visibility:', error);
+      }
+      if (!showButtonVerifyBill === false) console.log("Hided ButtonVerify")
+      else console.log("Unhided ButtonVerify")
+    };
+
+    fetchData();
+  }, [])
+
+
+
   return (
     <div className="bg-white">
       <div className="mx-auto max-w-2xl px-4 py-4 sm:px-6 sm:py-12 lg:max-w-7xl lg:px-8 cursor-pointer">
@@ -42,23 +64,28 @@ const Home = () => {
         </h2>
 
         <div className="mt-6 grid grid-cols-2 gap-4 sm:grid-cols-3 lg:grid-cols-6">
-          {listMenu.map((menu) => (
-            <a
-              key={menu.id}
-              onClick={() => handleNavigate(menu.href)}
-              className="group flex flex-col items-center rounded-lg bg-white p-4 shadow hover:shadow-lg hover:scale-105 transition"
-            >
-              <div className="w-20 h-20 mb-2">
-                <img
-                  src={menu.imageSrc}
-                  className="w-full h-full rounded-md object-cover"
-                />
-              </div>
-              <h3 className="text-sm font-medium text-gray-800 group-hover:text-blue-500">
-                {menu.name}
-              </h3>
-            </a>
-          ))}
+          {listMenu
+            .filter(menu => {
+              if (menu.id === 4 && !showButtonVerifyBill) return false;
+              return true;
+            })
+            .map((menu) => (
+              <a
+                key={menu.id}
+                onClick={() => handleNavigate(menu.href)}
+                className="group flex flex-col items-center rounded-lg bg-white p-4 shadow hover:shadow-lg hover:scale-105 transition"
+              >
+                <div className="w-20 h-20 mb-2">
+                  <img
+                    src={menu.imageSrc}
+                    className="w-full h-full rounded-md object-cover"
+                  />
+                </div>
+                <h3 className="text-sm font-medium text-gray-800 group-hover:text-blue-500">
+                  {menu.name}
+                </h3>
+              </a>
+            ))}
         </div>
       </div>
     </div>
