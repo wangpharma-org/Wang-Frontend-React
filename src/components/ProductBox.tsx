4eb6583636eb4a722b91e7fcc761c8519ce0bd8b
6710110400 SASIT SUANSADANG <155415427+Sasit-Nine@users.noreply.github.com>
2025-07-08 17:34:16 +0700

    Save

--- src/components/ProductBox.tsx
+++ src/components/ProductBox.tsx
@@ -230,11 +230,6 @@ export default function ProductBox({
               </p>
             )}
           </div>
-          {/* <div className="flex justify-end pr-1">
-                        <button className="border-gray-300 border rounded-sm px-5 py-2 shadow-md bg-blue-400 text-white">
-                          พิมพ์
-                        </button>
-                      </div> */}
         </div>
       </div>
     </div>
