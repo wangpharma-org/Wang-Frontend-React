43c14b7132952bf677bf45d4eca73ca0aaa4def8
XVPB511 <por4987@gmail.com>
2026-03-21 17:01:29 +0700

    fix: disable login button until authentication is processed

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -99,6 +99,7 @@ const Login = () => {
               <div>
                 <button
                   onClick={handleLogin}
+                  disabled
                   className="flex w-full cursor-pointer justify-center rounded-md bg-blue-500 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-blue-600 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
                 >
                   เข้าสู่ระบบ
